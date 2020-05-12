421cf3a480751797be030e2e8c9e0cef44efc691
GD7 <GD7@DESKTOP-K4S8IUC>
2020-05-12 12:03:02 +0900

    가계부 spring+maven+mvc 프로젝트생성

---
+++ cashbook/src/main/java/com/gdu/cashbook1/ServletInitializer.java
@@ -0,0 +1,19 @@
+package com.gdu.cashbook1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/*
+ * servlet API
+ * 1. servlet : 요청처리
+ * 2. filter : 요청전후 처리
+ * 3. listener : 이벤트 반응 처리
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CashbookApplication.class);
+	}
+
+}
