4cc8044d048c143bb4425e991fa7c6bfac534787
GD7 <GD7@DESKTOP-K4S8IUC>
2020-05-12 17:13:41 +0900

    가계부 spring 회원가입서비스, 맵퍼설정(회원가입쿼리작성) DB로 가입한 데이터 전송완료

--- cashbook/src/main/java/com/gdu/cashbook/controller/IndexController.java
+++ cashbook/src/main/java/com/gdu/cashbook/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+	// 메인 홈 
    @GetMapping("/index")
    public String index() {
       return "index";
--- cashbook/src/main/java/com/gdu/cashbook/controller/MemberController.java
+++ cashbook/src/main/java/com/gdu/cashbook/controller/MemberController.java
@@ -1,22 +1,28 @@
 package com.gdu.cashbook.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.gdu.cashbook.service.MemberService;
 import com.gdu.cashbook.vo.Member;
 
 @Controller
 public class MemberController {
+	@Autowired
+	private MemberService MemberService;
+	//회원가입 폼으로
 	@GetMapping("/addMember")
 	public String addMember() {
 		return "addMember";
 	}
-	
+	//회원가입완료 시 
 	@PostMapping("/addMember")
-	public String addMember(Member member) { //Comand 객체 , 도메인 객체
+	public String addMember(Member member) {
+		//회원가입 폼에서 값이 정상적으로 넘어오는지 확인
 		System.out.println(member.toString());
-		//System.out.println(member);
-		return "redirect:/index";
+		MemberService.addMember(member);
+		return "redirect:/index"; //완료 후 홈으로
 	}
 }
