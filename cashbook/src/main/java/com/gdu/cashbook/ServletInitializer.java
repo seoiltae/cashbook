2461b37c7e59f0365381f123b9bbbe7bd0cb4cb7
GD7 <GD7@DESKTOP-K4S8IUC>
2020-05-12 12:11:14 +0900

    20.05.12-IndexController.java 추가/template index 생성/

--- cashbook/src/main/java/com/gdu/cashbook/ServletInitializer.java
+++ cashbook/src/main/java/com/gdu/cashbook/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.gdu.cashbook1;
+package com.gdu.cashbook;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
