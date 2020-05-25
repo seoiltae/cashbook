32ca9fcd7bf90aea9641357bab353c2fb0bdf453
gd7 <gd7@DESKTOP-9O9J5S0>
2020-05-25 18:18:33 +0900

    [서일태] 가계부 입력 시 카테고리 추가 기능 구현 가계부 입력 시 직접입력 선택 후 새로 작성 시 새로 추가 구현 html에서 input타입의 value값으로 직접입력 선택 시 보임 미선택시 숨김 버튼 클릭시 벨류값 없앰

--- cashbook/src/main/java/com/gdu/cashbook/mapper/CategoryMapper.java
+++ cashbook/src/main/java/com/gdu/cashbook/mapper/CategoryMapper.java
@@ -7,4 +7,6 @@ import com.gdu.cashbook.vo.Category;
 @Mapper
 public interface CategoryMapper {
 	public List<Category> selectCategoryList(); //가계부 카테고리 리스트
+	public void insertCategoryOne(Category category); //가계부 카테고리 추가
+	public String selectCategory(Category category); // 가계부 카테고리 추가 시 중복 확인
 }
--- cashbook/src/main/java/com/gdu/cashbook/service/CashService.java
+++ cashbook/src/main/java/com/gdu/cashbook/service/CashService.java
@@ -37,9 +37,16 @@ public class CashService {
 		return map;
 	}
 	// 가계부 추가 액션
-	public int addCash(Cash cash) {
-		int row = cashMapper.insertCashOne(cash);
-		return row;
+	public int addCash(Cash cash, Category category) {
+		String check = categoryMapper.selectCategory(category);
+		if(check == null) {
+			categoryMapper.insertCategoryOne(category);
+			int row = cashMapper.insertCashOne(cash);
+			return row;
+		} else {
+			int row = cashMapper.insertCashOne(cash);
+			return row;
+		}
 	}
 	//가계부 삭제(혹시모를 트랜잭션 처리를 위해 void대신 int)
 	public int removeCash(Cash cash) {
@@ -52,9 +59,16 @@ public class CashService {
 		return cashMapper.selectCashNoByCash(cash);
 	}
 	//가계부 수정 액션
-	public int modifyCash(Cash cash) {
-		int row = cashMapper.updateCashOne(cash);
-		return row;
+	public int modifyCash(Cash cash,  Category category) {
+		String check = categoryMapper.selectCategory(category);
+		if(check == null) {
+			categoryMapper.insertCategoryOne(category);
+			int row = cashMapper.updateCashOne(cash);
+			return row;
+		} else {
+			int row = cashMapper.updateCashOne(cash);
+			return row;
+		}
 	}
 	//카테고리 목록 //가계부 입력 시 가계부 수정 시 카테고리 선택
 	public List<Category> getSelectCategoryList() {
