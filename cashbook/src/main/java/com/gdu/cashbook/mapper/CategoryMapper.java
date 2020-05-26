c06a374a70505ee5e603c00f18d35f5cfdf85829
sktsc <sktsc@192.168.1.136>
2020-05-27 02:21:24 +0900

    [서일태] 가계부 입력 및 수정 오류 해결 가계부 입력과 수정 시 오류 해결 html에서 폼으로 넘낄대 카테고리네임 2중넘김 해결

--- cashbook/src/main/java/com/gdu/cashbook/mapper/CategoryMapper.java
+++ cashbook/src/main/java/com/gdu/cashbook/mapper/CategoryMapper.java
@@ -8,5 +8,5 @@ import com.gdu.cashbook.vo.Category;
 public interface CategoryMapper {
 	public List<Category> selectCategoryList(); //가계부 카테고리 리스트
 	public void insertCategoryOne(Category category); //가계부 카테고리 추가
-	public String selectCategory(Category category); // 가계부 카테고리 추가 시 중복 확인
+	public Category selectCategory(Category category); // 가계부 카테고리 추가 시 중복 확인
 }
--- cashbook/src/main/java/com/gdu/cashbook/service/CashService.java
+++ cashbook/src/main/java/com/gdu/cashbook/service/CashService.java
@@ -37,18 +37,15 @@ public class CashService {
 		return map;
 	}
 	// 가계부 추가 액션
-	public int addCash(Cash cash) {
-		int row = cashMapper.insertCashOne(cash);
-		//	String check = categoryMapper.selectCategory(category);
-	/*	if(check == null) {
-			categoryMapper.insertCategoryOne(category);
+	public int addCash(Cash cash, Category category) {
+			Category check = categoryMapper.selectCategory(category);
 			int row = cashMapper.insertCashOne(cash);
+		if(check == null) {
+			categoryMapper.insertCategoryOne(category);
 			return row;
 		} else {
-			int row = cashMapper.insertCashOne(cash);
 			return row;
-		}*/
-		return row;
+		}
 	}
 	//가계부 삭제(혹시모를 트랜잭션 처리를 위해 void대신 int)
 	public int removeCash(Cash cash) {
@@ -61,18 +58,15 @@ public class CashService {
 		return cashMapper.selectCashNoByCash(cash);
 	}
 	//가계부 수정 액션
-	public int modifyCash(Cash cash) {
+	public int modifyCash(Cash cash, Category category) {
+		Category check = categoryMapper.selectCategory(category);
 		int row = cashMapper.updateCashOne(cash);
-	/*	String check = categoryMapper.selectCategory(category);
 		if(check == null) {
 			categoryMapper.insertCategoryOne(category);
-			int row = cashMapper.updateCashOne(cash);
 			return row;
 		} else {
-			int row = cashMapper.updateCashOne(cash);
 			return row;
-		} */
-		return row;
+		}
 	}
 	//카테고리 목록 //가계부 입력 시 가계부 수정 시 카테고리 선택
 	public List<Category> getSelectCategoryList() {
