fb2efeafbc739da14a91ac8f595a43be748de0fc
seoiltae <sktsch11@naver.com>
2020-06-09 18:03:45 +0900

    [서일태] 댓글 VO생성 댓글 VO타입 생성 나의 게시판에 댓글목록 출력 미완성

---
+++ cashbook/src/main/java/com/gdu/cashbook/vo/Comment.java
@@ -0,0 +1,45 @@
+package com.gdu.cashbook.vo;
+
+public class Comment {
+	private int commentNo;
+	private int boardNo;
+	private String memberId;
+	private String commentContents;
+	private String commentDate;
+	public int getCommentNo() {
+		return commentNo;
+	}
+	public void setCommentNo(int commentNo) {
+		this.commentNo = commentNo;
+	}
+	public int getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getCommentContents() {
+		return commentContents;
+	}
+	public void setCommentContents(String commentContents) {
+		this.commentContents = commentContents;
+	}
+	public String getCommentDate() {
+		return commentDate;
+	}
+	public void setCommentDate(String commentDate) {
+		this.commentDate = commentDate;
+	}
+	@Override
+	public String toString() {
+		return "Comment [commentNo=" + commentNo + ", boardNo=" + boardNo + ", memberId=" + memberId
+				+ ", commentContents=" + commentContents + ", commentDate=" + commentDate + "]";
+	}
+	
+}
